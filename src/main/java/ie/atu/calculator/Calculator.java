dfbcd96c7cd6bfe552d0da964500711097744eba
SeanATU <g00292129@atu.ie>
2022-11-22 16:54:17 +0000

    Added the add function for the Calculator

--- src/main/java/ie/atu/calculator/Calculator.java
+++ src/main/java/ie/atu/calculator/Calculator.java
@@ -1,6 +1,27 @@
 package ie.atu.calculator;
+import java.util.Scanner;
 
 public class Calculator {
+
+    public static void main(String[] args){
+
+        add();
+    }
+
+    public static void add(){
+
+        Scanner inputs = new Scanner(System.in);
+
+        System.out.println("Enter your first numbers");
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Enter your second numbers");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is: "+total);
+
+    }
 
 }
 
